f8074c9a48de91a410e45df55832b60e1e81a650
navneethsai <navneethsai.kozhipurath@students.fhnw.ch>
2021-01-10 18:58:44 +0100

    Update

---
+++ digipr-acrm-web/src/main/java/rocks/process/acrm/controller/LoginController.java
@@ -0,0 +1,13 @@
+package rocks.process.acrm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping(path= "/Login")
+public class LoginController {
+
+    @GetMapping
+    public String getLoginView(){return "Login.html";}
+
+}
