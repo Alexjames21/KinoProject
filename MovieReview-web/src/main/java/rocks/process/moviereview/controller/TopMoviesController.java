fc4c414b665addbbf22f30edf31302d9d8558b70
janwilhelmfhnw <72554234+janwilhelmfhnw@users.noreply.github.com>
2021-01-13 19:51:31 +0100

    fixes

--- MovieReview-web/src/main/java/rocks/process/moviereview/controller/TopMoviesController.java
+++ MovieReview-web/src/main/java/rocks/process/moviereview/controller/TopMoviesController.java
@@ -1,9 +1,13 @@
 
 package rocks.process.moviereview.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
 
 @Controller
 @RequestMapping(path = "/top_movies")
