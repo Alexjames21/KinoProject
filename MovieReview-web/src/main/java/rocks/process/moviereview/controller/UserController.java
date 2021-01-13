8fe3cb19391b0cc151cf8c9faf768bb9e6d658d5
janwilhelmfhnw <72554234+janwilhelmfhnw@users.noreply.github.com>
2021-01-13 14:46:27 +0100

    bugs

--- MovieReview-web/src/main/java/rocks/process/moviereview/controller/UserController.java
+++ MovieReview-web/src/main/java/rocks/process/moviereview/controller/UserController.java
@@ -11,29 +11,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-import rocks.process.moviereview.business.service.LoginService;
-import rocks.process.moviereview.data.domain.User;
+//import rocks.process.moviereview.business.service.LoginService;
+//import rocks.process.moviereview.data.domain.User;
 
 @Controller
 public class UserController {
 
-    @Autowired
-    private LoginService agentService;
+    //@Autowired
+    //private LoginService agentService;
 
     @GetMapping("/register")
     public String getRegisterView() {
         return "register.html";
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<Void> postRegister(@RequestBody User agent) {
-        try {
-            agentService.saveAgent(agent);
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, e.getMessage());
-        }
-        return ResponseEntity.ok().build();
-    }
+    //@PostMapping("/register")
+    //public ResponseEntity<Void> postRegister(@RequestBody User agent) {
+      //  try {
+        //    agentService.saveAgent(agent);
+   //     } catch (Exception e) {
+    //        throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, e.getMessage());
+    //    }
+    //    return ResponseEntity.ok().build();
+   // }
 
     @GetMapping("/error")
     public String getErrorView() {
